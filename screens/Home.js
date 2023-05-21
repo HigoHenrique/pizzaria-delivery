8a823854d489c130bf0f54767abc3e106c9c8da0
ErickBarbosa88 <barbosaerick88@gmail.com>
2023-05-21 18:33:51 -0300

    atualizando tela Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Container, Flex } from "native-base";
+import useUser from '../contexts/userContext';
+import Header from '../components/Header';
 
 const Home = () => {
+  const user = useUser(state => state.user);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>De principio temos o objetivo de criar um sistema de delivery para pizzarias, na qual o usuario ira fazer seu pedido e após concluir sera enviado para o Whatsapp da empresa o pedido completo </Text>
-    </View>
+    
+    <Container width='lg' justifyContent='center'>
+      <Header nome={user.nome}/>
+      <Flex>
+        
+      </Flex>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
-
-export default Home;
