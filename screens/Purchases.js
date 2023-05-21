66e0553f210de8cd22ee9f3c5369f9fcd9d8834d
HigoHenrique <99215544+HigoHenrique@users.noreply.github.com>
2023-05-21 16:03:01 -0300

    adicionando mais Screens que vão ser usadas.

---
+++ screens/Purchases.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Purchases = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>De principio temos o objetivo de criar um sistema de delivery para pizzarias, na qual o usuario ira fazer seu pedido e após concluir sera enviado para o Whatsapp da empresa o pedido completo </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default Purchases;
