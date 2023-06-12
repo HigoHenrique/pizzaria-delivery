ae487f305061873edb423f59af80d215305a428b
Vitor <jvmbacalhau@gmail.com>
2023-06-12 14:41:53 -0300

    Atualizando signup - Alert e lógica em cadastrar

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,131 +1,138 @@
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
-import { Box, Heading, VStack, FormControl, Input, Button, Center, NativeBaseProvider } from "native-base";
-import { createUser } from '../api/user.js'
+import {
+  Alert,
+  Box,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Button,
+  Center,
+  NativeBaseProvider,
+  IconButton,
+  CloseIcon // Importar o IconButton aqui
+} from 'native-base';
+import { createUser } from '../api/user.js';
 import { useNavigation } from '@react-navigation/native';
-import {useQuery} from '@tanstack/react-query';
-import {getUsers} from '../api/user.js'
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from '../api/user.js';
 
 const Signup = () => {
-
-  const {refetch} = useQuery({
-    queryKey: ["users"],
+  const { refetch } = useQuery({
+    queryKey: ['users'],
     queryFn: getUsers,
-  })
+  });
 
-  const [nome, setNome] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [endereco, setEndereco] = useState('')
-  const [telefone, setTelefone] = useState('')
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertStatus, setAlertStatus] = useState('');
+  const [alertTitle, setAlertTitle] = useState('');
   const navigation = useNavigation();
 
-  const cadastrar = async () =>{
-    novoUser = {
-      nome,
-      email,
-      endereco,
-      telefone,
-      senha: password,
+  const cadastrar = async () => {
+    if (nome && email && password && endereco && telefone) {
+      const novoUser = {
+        nome,
+        email,
+        endereco,
+        telefone,
+        senha: password,
+      };
+      try {
+        await createUser(novoUser);
+        refetch();
+        navigation.navigate('Login');
+      } catch {
+        console.log('erro ao cadastrar usuário!');
+        setShowAlert(true);
+        setAlertStatus('error');
+        setAlertTitle('Erro ao cadastrar usuário!');
+      }
+    } else {
+      console.log('Preencha todos os campos!');
+      setShowAlert(true);
+      setAlertStatus('error');
+      setAlertTitle('Preencha todos os campos!');
     }
-    try{
-    await createUser(novoUser)
-    refetch()
-    navigation.navigate('Login')
-    }catch{
-      console.log("erro ao cadastrar usuário!")
-    }
-  }
+  };
 
-  // return (
-  //   <View style={styles.background}>
-  //   <View style={styles.container}>
-  //     <Text style={styles.title} >Cadastro: </Text>
-  //     <TextInput
-  //       style={styles.input}
-  //       onChangeText={setNome}
-  //       value={nome}
-  //       placeholder="Nome"
-  //     />
-  //     <TextInput
-  //       style={styles.input}
-  //       onChangeText={setEmail}
-  //       value={email}
-  //       placeholder="exemplo@email.com"
-  //     />
-  //     <TextInput
-  //       style={styles.input}
-  //       onChangeText={setPassword}
-  //       value={password}
-  //       secureTextEntry={true}
-  //       placeholder="*********"
-  //     />
-  //     <TextInput
-  //       style={styles.input}
-  //       onChangeText={setEndereco}
-  //       value={endereco}
-  //       placeholder="Rua exemplo, N 101, Bairro "
-  //     />
-  //     <TextInput
-  //       style={styles.input}
-  //       onChangeText={setTelefone}
-  //       value={telefone}
-  //       placeholder="XX XXXXX-XXXX"
-  //     />
-  //     <Button
-  //       title="Cadastrar"
-  //       onPress={cadastrar}
-  //     />
-  //   </View>
-  //   </View>
-  // );
-  return <Center w="100%" h="88%">
-      <Box safeArea p="2" w="90%" maxW="290" py="1">
-        <Heading size="lg" color="coolGray.800" _dark={{
-        color: "warmGray.50"
-      }} fontWeight="semibold">
-          Bem-vindo
-        </Heading>
-        <Heading mt="1" color="coolGray.600" _dark={{
-        color: "warmGray.200"
-      }} fontWeight="medium" size="xs">
-          Cadastre-se para continuar!
-        </Heading>
-        <VStack space={3} mt="5">
-        <FormControl>
-            <FormControl.Label>Nome</FormControl.Label>
-            <Input placeholder="Nome" onChangeText={setNome}value={nome}/>
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Email</FormControl.Label>
-            <Input placeholder="exemplo@email.com" onChangeText={setEmail} value={email}/>
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Senha</FormControl.Label>
-            <Input type="password" placeholder="*********" onChangeText={setPassword} value={password}/>
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Endereço</FormControl.Label>
-            <Input placeholder="Rua exemplo, N 101, Bairro " onChangeText={setEndereco} value={endereco}/>
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Número de Telefone</FormControl.Label>
-            <Input placeholder="XX XXXXX-XXXX" onChangeText={setTelefone} value={telefone}/>
-          </FormControl>
-          <Button mt="2" colorScheme="indigo" onPress={cadastrar}>
-            Cadastrar
-          </Button>
-        </VStack>
-      </Box>
-    </Center>;
+  return (
+    <NativeBaseProvider>
+      <Center w="100%" h="88%">
+        <Box safeArea p="2" w="90%" maxW="290" py="1">
+          <Heading size="lg" color="coolGray.800" _dark={{ color: 'warmGray.50' }} fontWeight="semibold">
+            Bem-vindo
+          </Heading>
+          <Heading mt="1" color="coolGray.600" _dark={{ color: 'warmGray.200' }} fontWeight="medium" size="xs">
+            Cadastre-se para continuar!
+          </Heading>
+          <VStack space={3} mt="5">
+            <FormControl isRequired>
+              <FormControl.Label>Nome</FormControl.Label>
+              <Input placeholder="Nome" onChangeText={setNome} value={nome} />
+            </FormControl>
+            <FormControl isRequired>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input placeholder="exemplo@email.com" onChangeText={setEmail} value={email} />
+            </FormControl>
+            <FormControl isRequired>
+              <FormControl.Label>Senha</FormControl.Label>
+              <Input type="password" placeholder="*********" onChangeText={setPassword} value={password} />
+            </FormControl>
+            <FormControl isRequired>
+              <FormControl.Label>Endereço</FormControl.Label>
+              <Input placeholder="Rua exemplo, N 101, Bairro " onChangeText={setEndereco} value={endereco} />
+            </FormControl>
+            <FormControl isRequired>
+              <FormControl.Label>Número de Telefone</FormControl.Label>
+              <Input placeholder="XX XXXXX-XXXX" onChangeText={setTelefone} value={telefone} />
+            </FormControl>
+            <Button mt="2" colorScheme="indigo" onPress={cadastrar}>
+              Cadastrar
+            </Button>
+          </VStack>
+        </Box>
+      </Center>
+      {showAlert && (
+        <Alert
+          status={alertStatus}
+          variant="subtle"
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+          px={4}
+          py={3}
+          mt={4}
+        >
+          <Box flexShrink={1}>
+            <Alert.Icon />
+          </Box>
+          <Text fontSize="md" fontWeight="medium" flexShrink={1} ml={2}>
+            {alertTitle}
+          </Text>
+          <IconButton
+            icon={<CloseIcon size="3" />}
+            onPress={() => setShowAlert(false)}
+            _text={{ color: 'coolGray.600' }}
+            alignSelf="flex-start"
+          />
+        </Alert>
+      )}
+    </NativeBaseProvider>
+  );
 };
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   container: {
     flex: 1,
@@ -148,6 +155,5 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 });
-
 
 export default Signup;
